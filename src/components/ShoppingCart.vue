689c6e0d90eba6907bd81d049b9e4b1fc4deb345
aniilannah <hannaliina.m@gmail.com>
2024-11-26 10:22:56 +0200

    vaja muuta

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,57 +1,57 @@
 <template>
   <div class = "container mt-4">
-  <h1 class = "text-center mb-4">Shopping Cart</h1>
+    <h1 class = "text-center mb-4">Shopping Cart</h1>
 
-    <h2>Add Product</h2>
-    <div>
-      <input type="text" placeholder="Product Name">
-      <input type="text" placeholder="Price">
-      <input type="text" placeholder="Quantity">
-      <button>Add Product</button>
-    </div>
-    <h2>Cart Items</h2>
-    <div>
-      v-for="item in cartItems"
-    </div>
-    <button>Remove</button>
-    <br>
-    <p>3x Apple @ 0.80€</p>
-    <button>Remove</button>
-    <br>
-    <p>4x Pear @ 1.20€</p>
-    <button>Remove</button>
-    <div>
+   <table class="table">
+    <thead>
+    <tr>
+      <th>Product</th>
+      <th>Quantity</th>
+      <th>Price</th>
+      <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in cartItems" :key="item">
+            <td>{{item.name}}</td>
+            <td>{{item.quantity}}</td>
+            <td>{{item.price}}</td>
+          <td>
+          <button @click="removeProduct(item.name)" class="btn btn-danger btn-sm">Remove</button>
+          </td>
+      </tr>
+    </tbody>
+   </table>
 
-    </div>
-
-    <div class = "d-flex justify-content-between mt-4">
-      <h4>Total:</h4>
-      <h4>€{{cartTotal.toFixed(2)}}</h4>
-    </div>
+    <h3 class="mt-4">Total: €{{cartTotal.toFixed(2)}}</h3>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  data:() => ({
-    apibase: "http://localhost:9001/api/cart",   // port ei tohi frondis ja backis olla sama, kui on sama, siis backis saab application.properties uues porti määrata
-    cartTotal: 0
-  }),
-  mounted() {
-    axios
-        .get(`${this.apibase}/cart-total`)
-        .then((res) => (this.cartTotal = res.data))
-        .catch(console.error)
+  data: () => ({
+      api: "http://localhost:9001/api/cart",
+      // newProduct: { name: "", price: 0, quantity: 1 },
+      cartTotal: 0,
+      cartItems: [],
+    }),
 
-    axios
-        .get(`${this.apibase}/get-items`)
-        .then((res) => (this.cartItems = res.data))
-        .catch(console.error)
-  }
+  methods: {
+    fetchCart() {
+      axios.all([
+          axios.get(`${this.api}/cart-total`).then(res => (this.cartTotal = res.data)),
+          axios.get(`${this.api}/get-items`).then(res => (this.cartItems = res.data)),
+          ]);
+      },
+    },
+
+  mounted(){
+     this.fetchCart();
+  },
 };
-
 </script>
+
 
 <!--<style>-->
 <!--</style>-->
